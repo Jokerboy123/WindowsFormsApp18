8d96f57923ab48fddcee10d58abaadb6794f8ce5
Jokerboy123 <71408943+Jokerboy123@users.noreply.github.com>
2020-09-20 12:50:39 +0300

    Add files via upload

---
+++ WindowsFormsApp18/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string Alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя 0123456789";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = ori(textBox1.Text);
+        }
+        public string ori(string inp)
+        {
+            StringBuilder code = new StringBuilder();
+            string s = textBox1.Text;
+            string sd = textBox3.Text;
+            int step = Convert.ToInt32(sd);
+            for (int i = 0; i < s.Length; i++)
+                for (int j = 0; j < Alphabet.Length; j++)
+                    if (s[i] == Alphabet[j]) code.Append(Alphabet[j + step]%Alphabet.Length);
+            return code.ToString();
+
+        }
+    }
+}
